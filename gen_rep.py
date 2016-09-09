82f4ab6d044348cff98afbec54234ecc5aa4e06d
Peihong Guo <peihongguo@gmail.com>
2016-09-09 05:52:27 -0500

    added embedding generation.

---
+++ gen_rep.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2
+
+import time
+
+start = time.time()
+
+import argparse
+import cv2
+import itertools
+import os
+
+import numpy as np
+np.set_printoptions(precision=2)
+
+import openface
+
+fileDir = os.path.dirname(os.path.realpath(__file__))
+modelDir = os.path.join(fileDir, '..', 'models')
+dlibModelDir = os.path.join(modelDir, 'dlib')
+openfaceModelDir = os.path.join(modelDir, 'openface')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('imgs', type=str, nargs='+', help="Input images.")
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",
+                    default=os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'))
+parser.add_argument('--imgDim', type=int,
+                    help="Default image dimension.", default=96)
+parser.add_argument('--verbose', action='store_true')
+
+args = parser.parse_args()
+
+if args.verbose:
+    print("Argument parsing and loading libraries took {} seconds.".format(
+        time.time() - start))
+
+start = time.time()
+align = openface.AlignDlib(args.dlibFacePredictor)
+net = openface.TorchNeuralNet(args.networkModel, args.imgDim)
+if args.verbose:
+    print("Loading the dlib and OpenFace models took {} seconds.".format(
+        time.time() - start))
+
+
+def getRep(imgPath):
+    if args.verbose:
+        print("Processing {}.".format(imgPath))
+    bgrImg = cv2.imread(imgPath)
+    if bgrImg is None:
+        raise Exception("Unable to load image: {}".format(imgPath))
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+    if args.verbose:
+        print("  + Original size: {}".format(rgbImg.shape))
+
+    start = time.time()
+    bb = align.getLargestFaceBoundingBox(rgbImg)
+    if bb is None:
+        raise Exception("Unable to find a face: {}".format(imgPath))
+    if args.verbose:
+        print("  + Face detection took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+    alignedFace = align.align(args.imgDim, rgbImg, bb,
+                              landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+    if alignedFace is None:
+        raise Exception("Unable to align image: {}".format(imgPath))
+    if args.verbose:
+        print("  + Face alignment took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+    rep = net.forward(alignedFace)
+    if args.verbose:
+        print("  + OpenFace forward pass took {} seconds.".format(time.time() - start))
+        print("Representation:")
+        print(rep)
+        print("-----\n")
+    return rep
+
+reps = []
+for img in args.imgs:
+    print 'processing', img
+    rep = getRep(img)
+    reps.append(rep)
+
+with open('reps.txt', 'w') as f:
+    for r in reps:
+        f.write(' '.join([str(x) for x in r]) + '\n')
